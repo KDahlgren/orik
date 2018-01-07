6537afb8127c4da199a85b4b838050b4736c5902
KDahlgren <kdahlgren15@gmail.com>
2018-01-07 10:44:51 -0800

    updated examples and added newest versions of the elastic search tests.

--- examples/elastic_v3/run.sh
+++ examples/elastic_v3/run.sh
@@ -1,31 +1,8 @@
 #!/usr/bin/env bash
 
 
-cmd="time python ../../src/drivers/orik.py -c 0 -n a,b,c --EOT 4 -f ./elastic_v3.ded --evaluator c4"
-opt_cmd="cmd"
+cmd="time python ../../src/drivers/orik.py -c 1 -n a,b,c,C,G --EOT 15 --EFF 0 -f ./elastic_v3.ded --evaluator c4 --settings ./settings.ini"
 
 rm ./IR.db
-rm ./tmp.txt
-
-if [ "$1" = "$opt_cmd" ]
-then
-  # run command an do not hide stdout
-  $cmd
-  rm ./IR.db
-else
-  # run the test and hide stdout
-  $cmd > tmp.txt
-
-  # check if test passed (TODO: make more sophisticated)
-  if grep -Fxq "PROGRAM EXITED SUCCESSFULLY" tmp.txt
-  then
-    echo "TEST PASSED"
-    rm tmp.txt
-  else
-    echo "TEST FAILED"
-    echo -e "Please see ./tmp.txt for execution outputs, or run the following command for more info :\n$cmd"
-  fi
-fi
-
-# remove the temp file to clean up directory
-#rm tmp.txt
+$cmd
+rm ./IR.db
