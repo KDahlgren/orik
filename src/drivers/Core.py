d40956be0504ee2a701c955049ac89352826060c
KDahlgren <kdahlgren15@gmail.com>
2018-01-07 16:15:51 -0800

    cleaned up comments and formatting.

--- src/drivers/Core.py
+++ src/drivers/Core.py
@@ -41,8 +41,6 @@ class Core :
   #############
   argDict                  = None  # dictionary of commaned line args
   cursor                   = None  # a reference to the IR database
-  original_prog_lines_only = None  # dictionary of the original lines of the c4 program
-                                   # sans any rewrites
 
   # --------------------------------- #
 
@@ -64,7 +62,7 @@ class Core :
     logging.debug( "*******************************************************" )
 
     # ---------------------------------------------------------------- #
-    # 1. build c4 datalog program                                      #
+    # 1. build c4 datalog program (USING IAPYX)                        #
     # ---------------------------------------------------------------- #
 
     # allProgramData := [ allProgramLines, tableListArray ]
@@ -73,15 +71,15 @@ class Core :
     table_array    = allProgramData[1]
 
     # ----------------------------------------------- #
-    # 2. evaluate                                     #
+    # 2. evaluate (USING IAPYX)                       #
     # ----------------------------------------------- #
 
-    # use c4 wrapper 
+    # use c4 wrapper always 
     parsedResults = self.evaluate( self.argDict, allProgramData )
 
-    # ----------------------------------------------- #
-    # 3. get provenance tree                          #
-    # ----------------------------------------------- #
+    # ---------------------------------------------------- #
+    # 3. get provenance tree (THIS IS THE ORIK STUFF)      #
+    # ---------------------------------------------------- #
 
     provTreeComplete = self.buildProvTree( parsedResults, self.argDict[ "EOT" ], 0, self.cursor )
 
@@ -101,29 +99,43 @@ class Core :
   # return some data structure or storage location encompassing the evaluation results.
   def evaluate( self, argDict, allProgramData ) :
 
+    # ----------------------------------------------------------------- #
     # inject custom faults here.
+
     allProgramData = self.injectCustomFaults( argDict, allProgramData )
+
+    # ----------------------------------------------------------------- #
+    # run the program evaluation using c4
 
     results_array = c4_evaluator.runC4_wrapper( allProgramData )
 
     # ----------------------------------------------------------------- #
     # dump evaluation results locally
+
     eval_results_dump_dir = os.path.abspath( os.getcwd() ) + "/data/"
 
     # make sure data dump directory exists
     if not os.path.isdir( eval_results_dump_dir ) :
-      print "WARNING : evalulation results file dump destination does not exist at " + eval_results_dump_dir
-      print "> creating data directory at : " + eval_results_dump_dir
-      os.system( "mkdir " + eval_results_dump_dir )
-      if not os.path.isdir( eval_results_dump_dir ) :
-        tools.bp( __name__, inspect.stack()[0][3], "FATAL ERROR : unable to create evaluation results dump directory at " + eval_results_dump_dir )
-      print "...done."
 
-    # data dump directory exists
+      logging.debug( "WARNING : evalulation results file dump destination does not exist at " + eval_results_dump_dir )
+      logging.debug( "> creating data directory at : " + eval_results_dump_dir )
+
+      os.system( "mkdir " + eval_results_dump_dir )
+
+      if not os.path.isdir( eval_results_dump_dir ) :
+        tools.bp( __name__, inspect.stack()[0][3], "FATAL ERROR : unable to create evaluation \
+                                                                  results dump directory at " + \
+                                                                  eval_results_dump_dir )
+      logging.debug( "...done." )
+
+    # ----------------------------------------------------------------- #
+    # data dump directory exists by now, so perform the dump.
+
     self.eval_results_dump_to_file( results_array, eval_results_dump_dir )
 
     # ----------------------------------------------------------------- #
     # parse results into a dictionary
+
     parsedResults = tools.getEvalResults_dict_c4( results_array )
 
     # ----------------------------------------------------------------- #
@@ -163,13 +175,13 @@ class Core :
   
     logging.debug( "  BUILD PROV TREE : running process..." )
 
-    if parsedResults :
-      # 000000000000000000000000000000000000000000000000000000000000000000 #
-      # grab the set of post records at EOT.
-      # assume the right-most attribute/variable/field of the post schema
-      # represents the last send time (aka EOT).
-      # should be true b/c of the dedalus rewriter reqs for deductive rules.
+    # ------------------------------------------------------------------------------ #
+    # grab the set of post records at EOT.
+    # assume the right-most attribute/variable/field of the post schema
+    # represents the last send time (aka EOT).
+    # should be true b/c of the dedalus rewriter reqs for deductive rules.
 
+    if parsedResults :
       postrecords_all = parsedResults[ "post" ]
       postrecords_eot = []
     
@@ -187,41 +199,43 @@ class Core :
     
       logging.debug( "postrecords_eot = " + str(postrecords_eot) )
     
+      # ------------------------------------------------------------------------------ #
       # !!! BREAK EARLY IF POST CONTAINS NO EOT RECORDS !!!
+
       if len( postrecords_eot ) < 1 :
-        tools.bp( __name__, inspect.stack()[0][3], "FATAL ERROR : attempting to build a provenance tree when no post eot records exist. Aborting..." )
+        tools.bp( __name__, inspect.stack()[0][3], "FATAL ERROR : attempting to build \
+                                                    a provenance tree when no post eot \
+                                                    records exist. Aborting..." )
    
-      # 000000000000000000000000000000000000000000000000000000000000000000 #
-  
+    # ------------------------------------------------------------------------------ #
     # abort execution if evaluation results not accessible
+
     else :
       sys.exit( "ERROR: No access to evaluation results.\nAborting..." ) # sanity check
   
     # ------------------------------------------------------------------------------ #
     # there exist results and eot post records.
-    logging.debug( "\n~~~~ BUILDING PROV TREE ~~~~" )
+
+    logging.debug( "++++++++++++++++++++++++" )
+    logging.debug( "+  BUILDING PROV TREE  +" )
+    logging.debug( "++++++++++++++++++++++++" )
   
-    # ------------------------------------------------------------------------------ #
     # initialize provenance tree structure
     provTreeComplete = ProvTree.ProvTree( "FinalState", parsedResults, irCursor )
 
-    # ------------------------------------------------------------------------------ #
     # populate prov tree
     for seedRecord in postrecords_eot :
       logging.debug( " ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" )
       logging.debug( "           NEW POST RECORD " )
       logging.debug( "seedRecord = " + str( seedRecord ) )
       provTreeComplete.generateProvTree( "post", seedRecord )
-    # ------------------------------------------------------------------------------ #
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-    # KD : bcast node debugging session 6/21/17
-    #provTreeComplete.createGraph( None, iter_count )
-    #tools.bp( __name__, inspect.stack()[0][3], "built prov tree and created graph." )
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    # ------------------------------------------------------------------------------ #
+    # build the provenance graph
 
     default_fmla_index = 0 
     provTreeComplete.createGraph( None, default_fmla_index, iter_count, self.argDict )
+
     # ------------------------------------------------------------------------------ #
 
     return provTreeComplete
