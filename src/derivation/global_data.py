1eeda4771096253ecb62f3b9840b9172a1c7f8cb
KDahlgren <kdahlgren15@gmail.com>
2018-01-09 15:42:19 -0800

    added file for maintaining global data. mostly used for debugging.

---
+++ src/derivation/global_data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Use as a repository for global variables accessible at runtime.
+'''
+
+global total_number_of_descedants
+total_number_of_descendants = 0
+
+global total_descendant_list
+total_descendant_list = []
